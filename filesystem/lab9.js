6a68d25339cd7d94641687b45f96bc08d98f2036
harshitagrawal-indpro <harshitagra8092@gmail.com>
2025-05-28 11:55:52 +0530

    read the file

--- filesystem/lab9.js
+++ filesystem/lab9.js
@@ -2,3 +2,15 @@ const fs = require('fs');
 const path = require('path');
 
 // 1. read a text file 
+
+const filePath = path.join(__dirname,"text.txt");
+fs.readFile(filePath, 'utf-8', (err, data) => {
+    if(err){
+        console.log("error readign the file :", err);
+        return;
+    }
+    console.log(data);
+
+});
+
+
